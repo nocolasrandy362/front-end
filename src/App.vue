d084f01d8cec64a832fc729091ba769cecb2c93c
nocolasrandy362 <nicolasrandy362@gmail.com>
2025-05-14 10:16:01 +0800

    解释合并链表注释

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,15 @@ const buildSingleLinkList = (arr) => {
 const mergeList = () => {
   let ListA = buildSingleLinkList([11, 22, 31, 40, 41, 53]);
   let ListB = buildSingleLinkList([17, 32, 34, 38, 41, 50]);
-  let pA = ListA;
-  let pB = ListB;
+  let pA = ListA.next;
+  let pB = ListB.next;
   let C = buildSingleLinkList([])
   let pC = C
   while (pA && pB) {
-    if (pA.val > pB.val) {
+    /*NO: You are merging two ordered singly linked lists into an ascending linked list. 
+      But your judgment condition is pA.val > pB.val */
+    // if (pA.val > pB.val) { // No
+      if (pA.val < pB.val) { // OK
       // TypeError: Cannot set properties of null (setting 'next')
       // 你正在尝试给 null 设置一个属性（这里是 .next）
       pC.next = pA;
