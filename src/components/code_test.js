8cc596964fe89b84d8b0b359d24ae7501d17012d
nocolasrandy362 <nicolasrandy362@gmail.com>
2025-09-04 02:51:50 +0800

    LeetCode

---
+++ src/components/code_test.js
@@ -0,0 +1,11 @@
+// https://www.nowcoder.com/exam/oj?questionJobId=10&subTabName=online_coding_page
+
+// BM1 链表反转
+// BM2 链表内指定区间反转
+// BM3 链表中结点每K个一组翻转
+// BM4 合并两个排序的链表
+// BM6 判断链表中是否有环
+// BM7 链表中环的入口结点
+// BM8 链表中倒数K个结点
+// BM9 删除链表倒数第n个结点 
+// BM10 两个链表的第一个公共结点
